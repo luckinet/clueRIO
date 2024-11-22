a34507a477a100f04144eeeb8cfaf3e6a08f6d00
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-11-22 17:50:31 +0100

    build function body

--- R/clue_driver.R
+++ R/clue_driver.R
@@ -3,15 +3,56 @@
 #' @param scene description
 #' @param file description
 #' @param name description
-#' @details Additional details...
+#' @param ... additional arguments for for writing files with
+#'   \link[terra]{writeRaster}.
+#' @details
 #' @return
-#' @importFrom checkmate assertClass
+#' @examples
+#' @importFrom checkmate assertClass assertFileExists assertCharacter
+#' @importFrom dplyr first last
+#' @importFrom stringr str_split
+#' @importFrom terra writeRaster
 #' @export
 
-clue_driver <- function(scene, file, name = NULL){
+clue_driver <- function(scene, file, name = NULL, ...){
 
   assertClass(x = scene, classes = "scene")
+  assertCharacter(x = name, len = 1, any.missing = FALSE, null.ok = TRUE)
+  isRast <- testClass(x = file, classes = "SpatRaster")
 
+  root <- file.path(scene@meta$path)
+
+  # make sure it's a raster
+  if(!isRast){
+    assertFileExists(x = file, access = "rw")
+    driver <- rast(file)
+  } else {
+    driver <- file
+  }
+
+
+  if(length(file) == 1){
+
+    isDyn <- FALSE
+    if(is.null(name)){
+      name <- first(str_split(last(str_split(file, "/")[[1]]), "[.]")[[1]])
+    }
+
+  } else {
+
+    isDyn <- TRUE
+    if(is.null(name)){
+      stop("please provide a name for this dynmic driver.")
+    }
+
+  }
+
+  writeRaster(x = driver, filename = paste0(root, "input/", name, ".tif"),
+              overwrite = TRUE, ...)
+
+  temp <- list(dynamic = isDyn)
+
+  scene@drivers[[name]] <- temp
 
   return(scene)
 }
--- R/clue_landsystems.R
+++ R/clue_landsystems.R
@@ -1,21 +1,62 @@
 #' Set the initial land systems information
 #'
 #' @param scene description
-#' @param file description
-#' @param regions description
-#' @param cats description
-#' @details Additional details...
+#' @param map description
+#' @param cats (mention that either to provide this, or set it already before in
+#'   \code{map}).
+#' @param ... additional arguments for for writing files with
+#'   \link[terra]{writeRaster}.
+#' @details
 #' @return
-#' @importFrom checkmate assertClass
+#' @examples
+#' @importFrom checkmate assertClass testClass assertFileExists testNull
+#'   assertTRUE
+#' @importFrom sf write_sf
+#' @importFrom terra rast set.cats levels writeRaster
 #' @export
 
-clue_landsystems <- function(scene, file, regions, cats = NULL){
+clue_landsystems <- function(scene, file, cats = NULL, ...){
 
   assertClass(x = scene, classes = "scene")
+  isRast <- testClass(x = file, classes = "SpatRaster")
 
+  root <- file.path(scene@meta$path)
 
-  # ensure this has a raster-attribute table: addCats()
+  # make sure it's a raster
+  if(!isRast){
+    assertFileExists(x = file, access = "rw")
+    map <- rast(file)
+  } else {
+    map <- file
+  }
 
+  if(is.null(cats(map)[[1]])){
+    if(is.null(cats)){
+      stop("please ensure that 'map' has a valid category table - see 'terra::cats()' - or provide it via the argument 'cats = ...'.")
+    } else {
+      assertDataFrame(x = cats, types = c("integerish", "character"))
+
+      # ensure that cats has the correct values
+      mapVals <- unique(values(map, mat = FALSE))
+      assertTRUE(x = all(cats$ID %in% mapVals))
+      assertTRUE(x = all(mapVals %in% cats$ID))
+
+      set.cats(x = map, value = cats)
+    }
+  }
+
+  scene@landsystems <- levels(map)[[1]]
+
+  # set options according to this layer
+  opts <- getOption("clue")
+  opts$value[which(opts$var == "mapRows_n")] <- as.character(nrow(map))
+  opts$value[which(opts$var == "mapCols_n")] <- as.character(ncol(map))
+  opts$value[which(opts$var == "map_res")] <- as.character(res(map))
+  opts$value[which(opts$var == "map_xmin")] <- as.character(xmin(map))
+  opts$value[which(opts$var == "map_ymin")] <- as.character(ymin(map))
+  options(clue = opts)
+
+  writeRaster(x = map, filename = paste0(root, "input/landsystems.tif"), overwrite = TRUE, ...)
 
   return(scene)
 }
